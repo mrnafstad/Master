8c5977df4fc00003e79897998b66aa16fce9d83f
mrnafstad <mr.nafstad@gmail.com>
2018-02-20 08:50:28 +0100

    Implemented the correct function for r'' and that's all she wrote

--- Kode/3DEradius.py
+++ Kode/3DEradius.py
@@ -2,45 +2,150 @@ import numpy as np
 import matplotlib.pylab as mpl
 from scipy.integrate import odeint
 import sys
+from itertools import cycle
 
 H0 = 2.2685455*10**12	#km/s/Mpc
 
-def r(x, a, Omega_m0, Omega_K, Lambda, a_i):
+def r(x, y, Omega_m0, Omega_K, Lambda, r_i, delta_i):
 	r = x[0]
-	drda = x[1]
-
+	drdy = x[1]
+	a = np.exp(y)
 	rr = [[],[]]
-	rr[0] = drda
-	rr[1] = -1./(Omega_m0/a**3 + Omega_K/a**2 + Lambda/3.) * ( 2./a*(2./3.*Lambda - Omega_m0/a**3)*drda + 2./a**2*(Omega_m0/a**3 + Lambda/3.)\
-	 + 1./2./a**2*Omega_m0*a_i**3*r**4 - Lambda/(3.*H0**2*a**2)*r )			#Noe galt i [ 1./2./a**2*Omega_m0*a_i**3*r**4 - Lambda/(3.*H0**2*a**2)*r ], overflow og invalid value
+	rr[0] = drdy
+	rr[1] = (Omega_m0/a**3 + Omega_K/a**2 + Lambda)**(-2) * (r*(-Omega_m0*(1+delta_i)*(r_i/r)**3 + Lambda) + drdy/2.*a*(Omega_m0/a**3 - 2*Lambda)) 	
 
 	return rr
 
 eps = 1.63e-5
-N = 1000000
+N = 10000
 
-a = np.linspace(eps, 1.1, N)
+y0 = float(sys.argv[3])					#-2 might be a decent number, roughly -7 corresponds to recombination
 
-Lambda = float(sys.argv[2])
-Omega_m0 = float(sys.argv[3])
+y = np.linspace( y0, -1e-10, N)
 
-if len(sys.argv) == 4:
-	Omega_K = 1 - Omega_m0 - Lambda
-else:
-	Omega_K = float(sys.argv[4])
+Lambda = 0.7
+Omega_m0 = 0.3
 
-print Omega_K
+Omega_K = 1 - Omega_m0 - Lambda
+
+delta_i_max = float(sys.argv[1])
+
+delta_int = int(sys.argv[2])
+
+delta_i = np.linspace(0, delta_i_max, delta_int)
 
 r0 = 1.0
-drda0 = float(sys.argv[1])
+drdx0 = 0.1
 
-radius = odeint(r, [r0, drda0], a, args=(Omega_m0, Omega_K, Lambda, a[0]), mxstep=1000000000)#	, full_output=1)
+"""
+radius = odeint(r, [r0, drdx0], y, args=(Omega_m0, Omega_K, Lambda, r0, delta_i[0]), mxstep=1000000000)#	, full_output=1)
 
-mpl.plot(a, radius[:,1], "--c", linewidth = 0.75, label = "s(a)") 		#TypeError: tuple indices must be integers, not tuple
-mpl.xlabel("a(t)")
+mpl.plot(y, radius[:,1], "--c", linewidth = 0.75, label = r"Background ($\delta_i = 0$)") 		#TypeError: tuple indices must be integers, not tuple
+mpl.xlabel("x = ln(a(t))")
 mpl.ylabel(r"$s = \frac{r}{r_i}$")
 mpl.legend()
-mpl.yscale("log")
-mpl.xscale("log")
+#mpl.yscale("log")
+#mpl.xscale("log")
+"""
+
+if len(sys.argv) == 5:
+	#Run through different values of the initial overdensity and plot together
+	for i in range(len(delta_i)):
+		sofa = odeint(r, [r0, drdx0], y, args = (Omega_m0, Omega_K, Lambda, r0, delta_i[i]))
+
+		if i == 0:
+
+			mpl.plot(y, sofa[:,0], "-c", linewidth = 0.75, label = r"$\delta_{i} =$ %.1f" % delta_i[i])
+
+		else:
+
+			mpl.plot(y, sofa[:,0], "-.", linewidth = 0.75, label = r"$\delta_{i} =$ %.1f" % delta_i[i])
+
+
+	mpl.xlabel("x = ln(a)")
+	mpl.ylabel("r(a)")
+	mpl.legend()
+
+	mpl.title(r"$x_0 =$ %0.2f, varying values of $\delta_i$" % y0)
+
+	mpl.show()
+
+else:
+#Want to run through different values of y0 at
+
+
+
+	y0_arr = np.linspace(-8, -1, 10)
+
+
+
+	for i in range(len(y0_arr)):
+
+
+		delta__i = 1.686
+
+		y = np.linspace( y0_arr[i], -1e-10, N)
+		sofa = odeint(r, [r0, drdx0], y, args = (Omega_m0, Omega_K, Lambda, r0, delta__i))	
+
+		mpl.plot(y, sofa[:,0], "--", linewidth = 0.75, label = r"$x_0 =$ %.1f, $\delta_i =$ %.2f" % (y0_arr[i], delta__i))
+
+
+
+		delta__i = 0.0
+
+		y = np.linspace( y0_arr[i], -1e-10, N)
+		sofa = odeint(r, [r0, drdx0], y, args = (Omega_m0, Omega_K, Lambda, r0, delta__i))
+
+		mpl.plot(y, sofa[:,0], "-c", linewidth = 0.75)#, label = r"$x_0 =$ %.1f, $\delta_i =$ %.2f" % (y0_arr[i], delta__i))
+
+
+
+		delta__i = 3.0
+
+		y = np.linspace( y0_arr[i], -1e-10, N)
+		sofa = odeint(r, [r0, drdx0], y, args = (Omega_m0, Omega_K, Lambda, r0, delta__i))
+
+		mpl.plot(y, sofa[:,0], ":", linewidth = 0.75, label = r"$x_0 =$ %.1f, $\delta_i =$ %.2f" % (y0_arr[i], delta__i))
+
+		delta__i = 5.0
+
+		y = np.linspace( y0_arr[i], -1e-10, N)
+		sofa = odeint(r, [r0, drdx0], y, args = (Omega_m0, Omega_K, Lambda, r0, delta__i))
+
+		mpl.plot(y, sofa[:,0], "-.", linewidth = 0.75, label = r"$x_0 =$ %.1f, $\delta_i =$ %.2f" % (y0_arr[i], delta__i))
+
+
+	mpl.xlabel("x = ln(a)")
+	mpl.ylabel("r(a)")
+	mpl.legend()
+	mpl.title(r"$\delta_i =$ %.2f, varying values of $x_0$" % delta__i)
+	mpl.show()
+
+"""
+y0_arr = np.linspace(-8, -1, 10)
+
+lines = ["--", "-."]
+
+
+
+for i in range(len(delta_i)):
+
+	for j in range(len(y0_arr)):
+
+		for k in range(len(lines)):
+
+			y = np.linspace( y0_arr[j], -1e-10, N)
+
+			sofa = odeint(r, [r0, drdx0], y, args = (Omega_m0, Omega_K, Lambda, r0, delta_i[i]))
+
+			mpl.plot(y, sofa[:,0], lines[k], linewidth = 0.75, label = r"$\delta_{i} =$ %.1f" % delta_i[i])
+
+
+mpl.xlabel("x = ln(a)")
+mpl.ylabel("r(a)")
+mpl.legend()
+
+mpl.title(r"$x_0 =$ %0.2f, varying values of $\delta_i$" % y0)
 
 mpl.show()
+"""
